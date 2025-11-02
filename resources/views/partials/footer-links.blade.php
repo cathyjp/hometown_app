80296c51504a6fc568af27335796881600e4df60
cathyjp <p0_10q01k@yahoo.co.jp>
2025-11-02 10:57:37 +0900

    refine fotter layout details

---
+++ resources/views/partials/footer-links.blade.php
@@ -0,0 +1,10 @@
+<li>
+    <a href="https://relevantly.work/" target="_blank" rel="noopener noreferrer" class="external-link footer-link-furusato">
+        「ふるさと住民票®」オフィシャルサイト
+    </a>
+</li>
+<li>
+    <a href="https://www.town.hiraizumi.iwate.jp/" target="_blank" rel="noopener noreferrer" class="external-link">
+        平泉町ホームページ
+    </a>
+</li>
